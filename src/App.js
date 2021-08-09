8ed99d05cd9ae2940fcdeb012bc13bd582feedb4
sakeeo <ig.muhammadlim@gmail.com>
2021-08-09 16:59:14 +0800

    fixed Invinity Scroll

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import InvinityScroll from "./Component/InvinityScroll";
+import React from "react";
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import InvinityScroll from "./Pages/InvinityScroll";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/InvinityScroll">Infinity Scroll</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/"><Home/></Route>
+          <Route path="/InvinityScroll" component={InvinityScroll}></Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
 
 
-function App() {
+function Home() {
   return (
     <div>
-       <InvinityScroll />
+      <h2>This is Home</h2>
     </div>
   );
 }
 
-export default App;
+
